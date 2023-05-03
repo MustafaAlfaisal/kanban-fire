a3eeb172b0da52f2aa9741dcbe26cf68a380fac8
Mustafa <mustafamfaisal@gmail.com>
2023-05-04 00:23:04 +0300

    connect to firebase
    complete the Ui and connect the app to the backend

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+    apiKey: 'AIzaSyBJ01BsJLTJqN87ic7pGtf_B20_wI26IF4',
+    authDomain: 'kanbanfire-d9ccd.firebaseapp.com',
+    projectId: 'kanbanfire-d9ccd',
+    storageBucket: 'kanbanfire-d9ccd.appspot.com',
+    messagingSenderId: '350020691946',
+    appId: '1:350020691946:web:69a0ca68f1b14f06b67493',
+    },
+  };
+  
